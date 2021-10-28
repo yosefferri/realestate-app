5323df9c3f9804cf41a8f416cab9fad474c0da3b
yosefferri <joseferri225@gmail.com>
2021-10-28 14:12:19 -0300

    Create UserTableSeeder.php

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // Let's clear the users table first
+        //User::truncate();
+
+        $faker = Factory::create();
+
+        // Let's make sure everyone has the same password and 
+        // let's hash it before the loop, or else our seeder 
+        // will be too slow.
+        $password = Hash::make('root');
+
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@test.com',
+            'password' => $password,
+        ]);
+
+        // And now let's generate a few dozen users for our app:
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->freeEmail,
+                'password' => '1234',
+            ]);
+        }
+    }
+}
