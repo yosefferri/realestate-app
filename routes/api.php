0f0402d5d8c74d1a10ebb279187309ef07051e38
yosefferri <joseferri225@gmail.com>
2021-10-29 09:11:57 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HouseController;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+
+Route::prefix('guest')->group(function () {
+    Route::post('/registration', [UserController::class, 'userRegistration'])->name('user-register');;
+    Route::post('/signout', [UserController::class, 'signOut']);
+});
+    
 
 
-Route::post('registration', [AuthController::class, 'customRegistration']);
-Route::post('signout', [AuthController::class, 'signOut']);
 
-Route::post('auth/login', [AuthController::class, 'login']);
-Route::group(['middleware' => ['apiJWT']], function (){
+Route::group(['prefix' => 'user','middleware' => ['assign.guard:users','jwt.auth']], function (){
+//    Route::prefix('users')->group(function(){
+    Route::put('/{house}', [HouseController::class, 'buy']);
     Route::get('/users/houses', [HouseController::class, 'userHouses']);
-    Route::prefix('houses')->group(function () {
+    Route::post('/', [UserController::class, 'index']);
+    Route::get('/user/{id}', function ($id) {
+        return new UserResource(User::findOrFail($id));
+    });
+    });
+//});
+
+
+    Route::group(['prefix' => 'admin','middleware' => ['assign.guard:admins','jwt.auth']], function () {
+        Route::post('registration', [AdminController::class, 'adminRegistration']);
+        Route::post('signout', [AdminController::class, 'signOut']);
+        Route::post('login', [AdminController::class, 'login']);
         Route::get('/', [HouseController::class, 'index']);
-        Route::post('/', [HouseController::class, 'store']);
+        Route::post('/all', [HouseController::class, 'store']);
         Route::get('/{house}', [HouseController::class, 'show']);
         Route::put('/{house}', [HouseController::class, 'update']);
-        Route::delete('/{house}', [HouseController::class, 'destroy']);
+        Route::delete('/{house}', [deleteHouse::class, 'deleted_at']);
     });
-});
