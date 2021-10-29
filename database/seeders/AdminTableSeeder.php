19143b0c337f22b87ed4ec066e639882ae2ee1ce
yosefferri <joseferri225@gmail.com>
2021-10-29 09:12:12 -0300

    Create AdminTableSeeder.php

---
+++ database/seeders/AdminTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use app\models\Admin;
+use Illuminate\Database\Seeder;
+use Faker\Factory;
+use Illuminate\Support\Facades\Hash;
+
+
+class AdminTableSeeder extends Seeder
+{
+    public function run()
+    {
+    $password = Hash::make('root');
+
+    Admin::create([
+        'name' => 'Administrator',
+        'email' => 'admin@test.com',
+        'password' => $password
+        ]);
+    }
+}
