ba9da3e8403de3a61f36011fdf4abdb01d0e71a1
yosefferri <joseferri225@gmail.com>
2021-10-29 09:12:01 -0300

    Update UserTableSeeder.php

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -20,13 +20,13 @@ class UserTableSeeder extends Seeder
         // Let's make sure everyone has the same password and 
         // let's hash it before the loop, or else our seeder 
         // will be too slow.
-        $password = Hash::make('root');
+       // $password = Hash::make('root');
 
-        User::create([
+       /* User::create([
             'name' => 'Administrator',
             'email' => 'admin@test.com',
             'password' => $password,
-        ]);
+        ]);*/
 
         // And now let's generate a few dozen users for our app:
         for ($i = 0; $i < 10; $i++) {
