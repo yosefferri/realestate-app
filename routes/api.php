b2bf8b4a3a10c6259b07136c23a2f2a65718eb44
yosefferri <joseferri225@gmail.com>
2021-11-01 08:09:59 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,33 +7,31 @@ use App\Http\Controllers\HouseController;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 
-Route::prefix('guest')->group(function () {
+Route::prefix('home')->group(function () {
     Route::post('/registration', [UserController::class, 'userRegistration'])->name('user-register');;
-    Route::post('/signout', [UserController::class, 'signOut']);
+    
 });
     
 
 
 
 Route::group(['prefix' => 'user','middleware' => ['assign.guard:users','jwt.auth']], function (){
-//    Route::prefix('users')->group(function(){
     Route::put('/{house}', [HouseController::class, 'buy']);
     Route::get('/users/houses', [HouseController::class, 'userHouses']);
     Route::post('/', [UserController::class, 'index']);
     Route::get('/user/{id}', function ($id) {
         return new UserResource(User::findOrFail($id));
     });
+    Route::get('/logout', [UserController::class, 'logout']);
     });
-//});
-
 
     Route::group(['prefix' => 'admin','middleware' => ['assign.guard:admins','jwt.auth']], function () {
         Route::post('registration', [AdminController::class, 'adminRegistration']);
-        Route::post('signout', [AdminController::class, 'signOut']);
+        Route::post('logout', [AdminController::class, 'logout']);
         Route::post('login', [AdminController::class, 'login']);
-        Route::get('/', [HouseController::class, 'index']);
+        Route::get('/index', [HouseController::class, 'index']);
         Route::post('/all', [HouseController::class, 'store']);
-        Route::get('/{house}', [HouseController::class, 'show']);
-        Route::put('/{house}', [HouseController::class, 'update']);
-        Route::delete('/{house}', [deleteHouse::class, 'deleted_at']);
+        Route::get('/house/{id}', [HouseController::class, 'show']);
+        Route::put('/house/{id}', [HouseController::class, 'update']);
+        Route::delete('/house/{id}', [deleteHouse::class, 'deleted_at']);
     });
