b47b0115ac96565948882dddc1840cdb22deebf2
yosefferri <joseferri225@gmail.com>
2021-11-07 21:31:43 -0300

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,27 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
-use App\Models\Admin;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-/*
+
     public function __construct()
     {
         $this->middleware('auth:api', ['except' => ['login']]);
     }
-*/
 
-    public function login()   {
+
+    // public function login()   {
     
+    //     $credentials = request(['email', 'password']);
+
+    //     /if (  $token = auth('api')->attempt($credentials)) {
+    //         return response()->json(['error' => 'Unauthorized'], 401);
+    //     }
+    //     return $this->respondWithToken($token);
+    // }
+
+    public function login()
+    {
         $credentials = request(['email', 'password']);
 
-        if (  $token = auth('jwt')->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
-        dd('login');
-        return $this->respondWithToken($token);
+         if (!$token = auth()->guard('api')->attempt($credentials)) {
+             return response()->json(['error' => 'Unauthorized'], 401);
+         }
+          return $this->respondWithToken($token);
     }
     
     public function adminRegistration(Request $request)
@@ -53,17 +61,18 @@ class AdminController extends Controller
     }
 
 
+    protected function respondWithToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::factory()->getTTL() * 60,
+            'user' => auth()->user()
+        ]);
+    }
+
     public function refresh()
     {
         return $this->respondWithToken(Auth::refresh());
     }
 
-    protected function respondWithToken($token)
-    {
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => Auth::factory()->getTTL() * 60
-        ]);
-    }
 }
