4ab857e79b308e1f0115822c98d0445dfda03737
yosefferri <joseferri225@gmail.com>
2021-11-07 21:32:22 -0300

    Update HouseTableSeeder.php

--- database/seeders/HouseTableSeeder.php
+++ database/seeders/HouseTableSeeder.php
@@ -11,13 +11,10 @@ class HouseTableSeeder extends Seeder
 {
     public function run()
     {
-        // Let's truncate our existing records to start from scratch.
-       // Event::truncate();
 
         $faker = \Faker\Factory::create();
 
-        // And now, let's create a few articles in our database:
-        for ($i = 0; $i < 20; $i++) {
+            for ($i = 0; $i < 20; $i++) {
             House::create([
                 'address' => $faker->address(),
                 'price_buy' =>$faker->numberBetween($min = 10500, $max = 60000),
@@ -26,10 +23,9 @@ class HouseTableSeeder extends Seeder
                 'kitchen'=>$faker->numberBetween(1, 3),
                 'garage'=>$faker->numberBetween(0, 3),
                 'bathroom'=>$faker->numberBetween(1, 3),
-                'TypeContract'=>$faker->numberBetween(1, 3),
+                'type_contract'=>$faker->numberBetween(1, 3),
                 'country'=>$faker->country(),
                 'date' => $faker->date(),
-                'time' => $faker->time(),
                 'status' => 1
             ]);
         }
